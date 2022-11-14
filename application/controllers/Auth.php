a31837f3bd7f1f0d62d66e1e272536647376d5b1
Jamuruu <diasspratamaa24@gmail.com>
2022-11-14 10:02:34 +0700

    edit: Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,7 +3,44 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-	public function index() {
-		$this->load->view('view-login');
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = "KampusKita Login";
+			$this->load->view('templates/header', $data);
+			$this->load->view('view-login.php');
+			$this->load->view('templates/footer');
+		} else {
+			$this->_login();
+		}
+	}
+
+	private function _login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		$admin = $this->db->get_where('admin', ['username' => $username])->row_array();
+
+		if ($admin) {
+			if ($password == $admin['password']) {
+				redirect('home');
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password!</div>');
+				redirect('auth');
+			}
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Username!</div>');
+			redirect('auth');
+		}
 	}
 }
